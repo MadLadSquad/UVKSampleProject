bd36faafd4cbbce39b6f0cc6e40d8f740427ef87
Madman10K <contact@madman10k.online>
2020-12-12 17:13:35 +0200

    Added example asset files

---
+++ Source/LeActor.cpp
@@ -0,0 +1,16 @@
+#include "LeActor.hpp"
+
+void UVK::LeActor::Tick(float DeltaTime)
+{
+
+}
+
+void UVK::LeActor::EndPlay()
+{
+
+}
+
+void UVK::LeActor::BeginPlay()
+{
+
+}
---
+++ Source/LeActor.hpp
@@ -0,0 +1,13 @@
+#include "Game.hpp"
+
+namespace UVK
+{
+	class LeActor : AActor
+	{
+	public:
+		virtual void Tick(float DeltaTime) override;	
+		virtual void BeginPlay() override;
+		virtual void EndPlay() override;
+
+	};
+}
