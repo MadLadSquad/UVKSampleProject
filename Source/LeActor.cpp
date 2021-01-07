bbd3875862a66c5a31b1212c7736469f9359dc67
Stanislav Vasilev <4ipsdaqde6@gmail.com>
2021-01-07 14:59:46 +0200

    Update LeActor.cpp

--- Source/LeActor.cpp
+++ Source/LeActor.cpp
@@ -1,16 +1,16 @@
 #include "LeActor.hpp"
 
-void UVK::LeActor::Tick(float DeltaTime)
-{
+//void UVK::LeActor::Tick(float DeltaTime)
+//{
 
-}
+//}
 
-void UVK::LeActor::EndPlay()
-{
+//void UVK::LeActor::EndPlay()
+//{
 
-}
+//}
 
-void UVK::LeActor::BeginPlay()
-{
+//void UVK::LeActor::BeginPlay()
+//{
 
-}
+//}
