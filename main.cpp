139b548c5ff690c4775de0a3d514a14e712e365b
Stanislav Vasilev <4ipsdaqde6@gmail.com>
2021-01-10 19:38:48 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,46 @@
 #include "Engine/Engine.hpp"
 
+class obj : public UVK::ScriptableObject
+{
+public:
+    obj()
+    {
+        events.createObj(this);
+    }
+
+    virtual void beginPlay() override
+    {
+        logger.consoleLog("Begin play", WARNING);
+    }
+
+    virtual void tick(float deltaTime) override
+    {
+        logger.consoleLog("Tick", ERROR);
+    }
+
+    virtual void endPlay() override
+    {
+        logger.consoleLog("End play", SUCCESS);
+    }
+};
+
 int main()
 {
-    UVK::GLRenderer rd;
-    rd.createWindow();
+    //Test tst;
+    //tst.open("test.yaml");
+
+    //pool.each([&](Actor act){
+    //    auto& a = registry.getComponent<UVK::CoreComponent>(act);
+
+    //    logger.consoleLogComplex<std::string>("Actor with name:", WARNING, { a.name, "and ID:", std::to_string(a.id) });
+
+    //});
+
+    //UVK::GLRenderer rd;
+    //rd.createWindow();
+    obj ob;
+
+    events.callBegin();
+    events.callTick(1.0f);
+    events.callEnd();
 }
